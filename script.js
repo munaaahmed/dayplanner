d2b8bf4c9ed0737a3247650fe55de4985ac8d4ab
Muna Ahmed <munaahmed@Munas-MacBook-Air.local>
2020-07-17 12:54:42 -0700

    another commit

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-$(function(){
-    $(document).om('click', "edit_btn" , function(){
-        let editable = $(this).prev('.edit_cont').attr('contentedtable');
-        if(editable){
-            $(this).text('edit');
-            $(this).prev('.edit_cont').removeAttr('contenteditable')
-            $(this).prev('.edit_cont').removeClass('edit_cont_border')
-        }
-        else{
-            $(this).text('save');
-            $(this).prev('.edit_cont').attr('ontentedtable', 'true');
-            $(this).prev('edit_cont').addClass('edit_cont_border');
-        }
-    })
-});
+$(document).ready(function() {
+  var currentday= moment().hours();
+    // test flag
+   test = false;
+
+    // get times from moment
+   $("#currentday").text(momet().format('MMMM Do YYYY'))})
