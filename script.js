1ae1449dd8116c377ae32b294cdb8264438b85ee
Muna Ahmed <munaahmed@Munas-MacBook-Air.local>
2020-07-21 21:52:09 -0700

    script commit

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
+var saveBtn = document.getElementById("#saveBtn");
+  var textarea = document.getElementById("#textarea1")
+
 $(document).ready(function() {
-  var currentday= moment().hours();
+
+  $("*[data-store]").each(function () {
+    $(this).val(localStorage.getItem("item-" + $(this).attr("data-store")));
+  });
+  $("#saveBtn1").on("click", function (itm) {
+    localStorage.setItem ("item-" + $("#textarea1").attr("data-store"), textarea1.value);
+  });
+  $("*[data-store]").each(function () {
+    $(this).val(localStorage.getItem("item-" + $(this).attr("data-store")));
+  });
+  $("#saveBtn2").on("click", function (itm) {
+    localStorage.setItem ("item-" + $("#textarea2").attr("data-store"), textarea2.value);
+  });
+
+
+});
+  
+
+
+
+
+
+var currentday = new Date();
+  var format='LLLL';
+  var result= moment(currentday).format(format);
     // test flag
    test = false;
 
     // get times from moment
-   $("#currentday").text(momet().format('MMMM Do YYYY'))})
+   $("#currentday").append(result);
+
